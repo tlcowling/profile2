19ededc848aa68b13502ecfc30206ef44b850afd
Thomas Cowling <tcow@fastmail.com>
2019-04-04 20:40:06 +0100

    faffery

---
+++ precache-manifest.953edfd58537e785728751b61e1bf3bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "567c3ca7f852bac0b450",
+    "url": "/static/css/main.30c7a193.chunk.css"
+  },
+  {
+    "revision": "567c3ca7f852bac0b450",
+    "url": "/static/js/main.743eddfd.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "b1cefaaf523567b24ce1ff64937402df",
+    "url": "/index.html"
+  }
+];
