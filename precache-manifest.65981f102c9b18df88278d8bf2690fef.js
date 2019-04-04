50ba9a67af12d1d38fae7a801b202b2084198374
Thomas Cowling <tcow@fastmail.com>
2019-04-04 21:16:47 +0100

    Updates

---
+++ precache-manifest.65981f102c9b18df88278d8bf2690fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e95b886359ede28511db",
+    "url": "/profile2/static/css/main.30c7a193.chunk.css"
+  },
+  {
+    "revision": "e95b886359ede28511db",
+    "url": "/profile2/static/js/main.4deffff7.chunk.js"
+  },
+  {
+    "revision": "0725850b9c7604bc757a",
+    "url": "/profile2/static/js/runtime~main.3d45bb65.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/profile2/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "246eb46d02facabcf5bda0d5b735da8c",
+    "url": "/profile2/index.html"
+  }
+];
